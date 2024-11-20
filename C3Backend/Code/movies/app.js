78e01ae6264395c5b65a17ace547152be4cff94b
吳介 <jaywu3272@gmail.com>
2024-11-20 02:31:25 +0000

    learn Route

--- C3Backend/Code/movies/app.js
+++ C3Backend/Code/movies/app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('express app for movies')
+app.get('/',(req,res) =>{
+    res.redirect('/movies')
+})
+
+app.get('/movies', (req, res) => {
+  res.send('listing movies')
+})
+
+app.get('/movie/:id',(req,res)=>{
+    const id = req.params.id
+    res.send(`read movie: ${id}`)
 })
 
 app.listen(port, () => {
